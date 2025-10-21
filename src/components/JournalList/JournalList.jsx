31a74515e10ec1fae1936e3a6f0f7603459eee81
Pavel Fedotov <samasyt@mail.ru>
2025-10-21 21:07:44 +0300

    feat: added useMemo in JournalList ( 9.8 )

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
 import './JournalList.css';
@@ -13,6 +13,9 @@ function JournalList({ items }) {
       return -1;
     }
   };
+  const filteredItems = useMemo(() => {
+    return items.filter((el) => el.userId === userId).sort(sortItems);
+  }, [items, userId]);
 
   if (items.filter((el) => el.userId === userId).length === 0) {
     return <p>Записей пока нет, добавьте новую!</p>;
@@ -20,16 +23,13 @@ function JournalList({ items }) {
 
   return (
     <>
-      {items
-        .filter((el) => el.userId === userId)
-        .sort(sortItems)
-        .map((el) => {
-          return (
-            <CardButton key={el.id}>
-              <JournalItem title={el.title} post={el.post} date={el.date} />
-            </CardButton>
-          );
-        })}
+      {filteredItems.map((el) => {
+        return (
+          <CardButton key={el.id}>
+            <JournalItem title={el.title} post={el.post} date={el.date} />
+          </CardButton>
+        );
+      })}
     </>
   );
 }
